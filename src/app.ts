b011cb1e208a6ee38a5085fa37ad7742cbfbd787
Hasan Ali <hans1998ali@gmail.com>
2023-09-12 10:01:35 +0530

    Setup error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,29 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import { HttpError } from 'http-errors';
+import { logger } from './config/logger';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) =>
-    res.json({ message: 'Welcome to auth service' }),
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Welcome to auth service' });
+});
+
+// error middleware
+app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (err: HttpError, req: Request, res: Response, next: NextFunction): void => {
+        logger.error(err.message);
+        res.status(err.statusCode).json({
+            name: err.name,
+            statusCode: err.statusCode,
+            message: err.message,
+            details: [],
+        });
+    },
 );
 
 export default app;
